6e6039b899b318dbd3e86ba77b2d7a585e1c7aab
Alec Stein <alec@dolthub.com>
2022-09-01 17:43:16 -0400

    added anthem parser

--- transparency_in_coverage_filesizes/README.md
+++ transparency_in_coverage_filesizes/README.md
@@ -1,1 +1,33 @@
 # transparency-in-coverage-filesizes
+
+# Cigna
+
+No data available
+
+# Anthem
+
+Files downloaded by going to their main page:
+
+https://www.anthem.com/machine-readable-file/search/
+
+and downloading their index. I then split this using jsplit, our in-house tool, to makeit into JSONL format. Then I streamed the lines.
+
+# Humana
+
+Scraper included.
+
+# UnitedHealthcare
+
+Scraper included.
+
+# Aetna
+
+Scraper included.
+
+# EmpireBC
+
+Scraper included.
+
+# BCBS North Carolina
+
+Scraper included, but the largest JSON file is malformed.
---
+++ transparency_in_coverage_filesizes/parse_anthem.py
@@ -0,0 +1,46 @@
+import glob
+import json
+import sqlite3
+from tqdm import tqdm
+
+con = sqlite3.connect("anthem_data.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS in_network_files(url PRIMARY KEY UNIQUE, size)")
+
+files = glob.glob('./2022-09-01_anthem_index_json/*')
+
+urls = set()
+
+print("Loading JSONL files...")
+for file in tqdm(files):
+	with open(file) as f:
+		for line in f:
+			data = json.loads(line)
+			in_network_files = data['in_network_files']
+			for in_network_file in in_network_files:
+				urls.add(in_network_file['location'])
+	# print("Gathered {len(urls)} URLs so far...")
+
+async def fetch_url_sizes(table, urls):
+    """
+    Simple async function for getting all the file sizes in a list of urls
+    and writing those to a SQLite table
+    """
+    
+    session = aiohttp.ClientSession()
+    fs = [session.head(url) for url in urls]
+
+    for f in asyncio.as_completed(fs):
+        resp = await f
+        url = str(resp.url)
+        size = int(resp.headers.get("content-length", -1))
+        cur.execute(f"""INSERT OR IGNORE INTO {table} VALUES ("{url}", {size})""")
+        con.commit()
+        
+    await session.close()
+
+print(f"Fetching {len(urls)} URLs and their sizes...")
+asyncio.run(fetch_url_sizes("in_network_files", urls))
+total = cur.execute("SELECT SUM(size) FROM in_network_files").fetchone()[0]
+
+print(f"Total filesize in GB: {total//1_000_000_000}")
