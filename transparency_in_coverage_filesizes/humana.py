faef4d6ccac3aeaa095241dac5683607fe4d7376
Josh Cardenzana <jvinniec@gmail.com>
2022-10-25 22:17:41 -0400

    Fix humana data pull to fetch data in batches of 2000. Include counter and percentage complete.

--- transparency_in_coverage_filesizes/humana.py
+++ transparency_in_coverage_filesizes/humana.py
@@ -7,18 +7,29 @@ cur.execute("CREATE TABLE IF NOT EXISTS in_network_files(url PRIMARY KEY UNIQUE,
 
 params = {
     "fileType": "innetwork",
-    "iDisplayLength": "500000",
+    "iDisplayLength": "2000",
+    "iDisplayStart": 0
 }
 
 print("Fetching URLs and their sizes...")
-resp = requests.get("https://developers.humana.com/Resource/GetData", params = params)
 
-for file in resp.json()["aaData"]:
-    url = "https://developers.humana.com/Resource/DownloadPCTFile?fileType=innetwork&" + file["name"]
-    size = int(file["size"])
-    cur.execute(f"""INSERT OR IGNORE INTO in_network_files VALUES ("{url}", {size})""")
+finished = False
+while not finished:
+    resp = requests.get("https://developers.humana.com/Resource/GetData", params = params) \
+                   .json()
+    if len(resp['aaData']) > 0:
+        for file in resp["aaData"]:
+            url = "https://developers.humana.com/Resource/DownloadPCTFile?fileType=innetwork&" + file["name"]
+            size = int(file["size"])
+            cur.execute(f"""INSERT OR IGNORE INTO in_network_files VALUES ("{url}", {size})""")
+    else:
+        finished = True
 
-con.commit()
+    con.commit()
+    
+    # Update the counter to get the next batch
+    params['iDisplayStart'] += len(resp['aaData'])
+    print(f"\r{params['iDisplayStart']: 6d}/{resp['iTotalRecords']} ({100*params['iDisplayStart']/resp['iTotalRecords']:3.1f}%)", end='')
 
 total = cur.execute("SELECT SUM(size) FROM in_network_files").fetchone()[0]
-print(f"Total filesize in GB: {total//1_000_000_000}")
+print(f"\nTotal filesize in GB: {total//1_000_000_000}")
